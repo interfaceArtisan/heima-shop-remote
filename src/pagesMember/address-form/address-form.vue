4fc434106eb5dd4baa423a71e3ec0ea50cf4b998
pengxing <979698753@qq.com>
2024-07-24 19:41:51 +0800

    地址新建页面

---
+++ src/pagesMember/address-form/address-form.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="address-form">address-form</div>
+</template>
+
+<script lang="ts" setup>
+const query = defineProps<{
+  id?: string
+}>()
+
+uni.setNavigationBarTitle({
+  title: query.id ? '修改地址' : '新建地址',
+})
+</script>
+
+<style lang="scss" scoped></style>
